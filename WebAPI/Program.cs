b443531877ee959cbe3e48b0ea3f32c12308d6ba
arseny.nikitin <nikitin.arseny@gmail.com>
2026-02-25 11:33:35 +0100

    additional init

---
+++ WebAPI/Program.cs
@@ -0,0 +1,41 @@
+using AppCore.Interfaces;
+using Infrastructure.Memory;
+
+namespace WebAPI;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddAuthorization();
+        builder.Services.AddSingleton<ICustomerService, MemoryCustomerService>();
+
+        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+        builder.Services.AddOpenApi();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        
+
+        app.MapGet("/api/customers", (ICustomerService service, HttpContext httpContext) =>
+            {
+                return service.GetCustomers();
+            })
+            .WithName("GetWeatherForecast");
+
+        app.Run();
+    }
+}
