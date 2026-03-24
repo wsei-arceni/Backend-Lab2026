2d3c0a3560124cf42fa0edda0b0d9949e7555fb0
Mr. Arceni <nikitin.arceny@gmail.com>
2026-03-24 10:39:34 +0100

    Expand repository interfaces

--- AppCore/Interfaces/ICompanyRepository.cs
+++ AppCore/Interfaces/ICompanyRepository.cs
@@ -4,5 +4,7 @@ namespace AppCore.Interfaces;
 
 public interface ICompanyRepository: IGenericRepositoryAsync<Company>
 {
-    
+    public List<Company> GetCompaniesByName(string name);
+    public Company GetCompanyByNIP(string nip);
+    public List<Person> GetCompanyMembers(Company company);
 }
--- AppCore/Interfaces/IOrganizationRepository.cs
+++ AppCore/Interfaces/IOrganizationRepository.cs
@@ -1,8 +1,10 @@
 ﻿using AppCore.Models;
+using AppCore.ValueObjects;
 
 namespace AppCore.Interfaces;
 
 public interface IOrganizationRepository: IGenericRepositoryAsync<Organization>
 {
-    
+    public List<Organization> GetOrganizationByType(OrganizationType type);
+    public List<Person> GetOrganizationMembers(Organization organization);
 }
