2c1205ef5fa37f4716286d46eaa63f5088d7022d
Mr. Arceni <nikitin.arceny@gmail.com>
2026-03-24 23:48:56 +0100

    Remove unused method

--- AppCore/Interfaces/IGenericRepositoryAsync.cs
+++ AppCore/Interfaces/IGenericRepositoryAsync.cs
@@ -6,7 +6,6 @@ namespace AppCore.Interfaces;
 public interface IGenericRepositoryAsync<T> where T : class
 {
     Task<T?> GetByIdAsync(Guid id);
-    Task<T?> FindByIdAsync(Guid id);
     Task<IEnumerable<T>> FindAllAsync();
     Task<PagedResult<T>> FindPagedAsync(int page, int pageSize);
     Task<T> AddAsync(T entity);
